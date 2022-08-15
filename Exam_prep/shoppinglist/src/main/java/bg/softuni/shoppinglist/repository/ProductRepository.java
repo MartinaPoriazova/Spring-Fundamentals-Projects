60780f4d118a2ef1a3f087f03c99f415972b2627
mporyazova <mporyazova@yahoo.com>
2022-08-15 22:19:39 +0300

    ShoppingList app - initial commit

---
+++ Exam_prep/shoppinglist/src/main/java/bg/softuni/shoppinglist/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package bg.softuni.shoppinglist.repository;
+
+import bg.softuni.shoppinglist.model.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
