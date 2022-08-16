5781ccc2f33a427f9679b85c47acde9106eb24d7
mporyazova <mporyazova@yahoo.com>
2022-08-16 16:02:13 +0300

    ProductDTO and ProductMapper added

---
+++ Exam_prep/shoppinglist/src/main/java/bg/softuni/shoppinglist/model/mapper/ProductMapper.java
@@ -0,0 +1,11 @@
+package bg.softuni.shoppinglist.model.mapper;
+
+import bg.softuni.shoppinglist.model.dto.ProductDTO;
+import bg.softuni.shoppinglist.model.entity.Product;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+
+    Product productDTOToProduct(ProductDTO productDTO);
+}
